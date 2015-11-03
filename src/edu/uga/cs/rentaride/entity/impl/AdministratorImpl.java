864d49783db35b21710610dbf598a1d2c7c15d9d
lclapper <lclapper@uga.edu>
2015-11-03 13:16:56 -0500

    Update and rename Administrator.java to AdministratorImpl.java
    I don't know if this is right.

---
+++ src/edu/uga/cs/rentaride/entity/impl/AdministratorImpl.java
@@ -0,0 +1,15 @@
+package edu.uga.cs.rentaride.entity.impl;
+
+import edu.uga.cs.rentaride.entity.RARException;
+import edu.uga.cs.rentaride.entity.Administrator;
+import edu.uga.cs.rentaride.entity.User;
+import edu.uga.cs.rentaride.persistence.impl.Persistent;
+
+/** This class represents an Administrator user.  It has no additional attributes, and all are inherited from User.
+ *
+ */
+public class AdministratorImpl
+    extends User
+    extends Persistent
+{
+}

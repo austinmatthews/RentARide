4a0c2b69947419c901faf4d6fbdcbe6fa9ed7d3f
bfuster1 <bfuster1@uga.edu>
2015-11-04 13:29:15 -0500

    Update HourlyPriceImpl.java

--- src/edu/uga/cs/rentaride/entity/impl/HourlyPriceImpl.java
+++ src/edu/uga/cs/rentaride/entity/impl/HourlyPriceImpl.java
@@ -8,7 +8,8 @@ import edu.uga.cs.rentaride.persistence.Persistable;
  *
  */
 public interface HourlyPrice
-    extends Persistable
+    extends Persistent 
+    implements HourlyPrice
 {
     /** Return the minimum hours for this price setting.
      * @return the minimum hours for this price setting

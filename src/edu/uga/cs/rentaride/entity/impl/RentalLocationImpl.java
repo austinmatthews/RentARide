2bd66c874a5f6735bf8be63457e7a67cc1803664
bfuster1 <bfuster1@uga.edu>
2015-11-04 23:30:00 -0500

    Update RentalLocationImpl.java

--- src/edu/uga/cs/rentaride/entity/impl/RentalLocationImpl.java
+++ src/edu/uga/cs/rentaride/entity/impl/RentalLocationImpl.java
@@ -1,4 +1,4 @@
-package edu.uga.cs.rentaride.entity;
+package edu.uga.cs.rentaride.entity.impl;
 
 
 import edu.uga.cs.rentaride.persistence.Persistable;
@@ -6,36 +6,54 @@ import edu.uga.cs.rentaride.persistence.Persistable;
 /** This class represents a rental location in the Rent-A-Ride system.
  *
  */
-public interface RentalLocation
-    extends Persistable
+public class RentalLocationImpl
+    extends Persistent
+    implements RentalLocation
 {
+    // RentalLocation attributes
+    private String name;
+    private String address;
+    private int capacity;
+    
     /** Return the name of this rental location
      * @return the name of this rental location
      */
-    public String getName();
+    public String getName(){
+        return name;
+    }
     
     /** Set the name of this rental location
      * @param name the new name of this rental location
      */
-    public void setName( String name );
+    public void setName( String name ){
+        this.name = name;
+    }
     
     /** Return the address of this rental location
      * @return the address of this rental location
      */
-    public String getAddress();
+    public String getAddress(){
+        return address;
+    }
     
     /** Set the address of this rental location
      * @param address the new address for this rental location
      */
-    public void setAddress( String address );
+    public void setAddress( String address ){
+        this.address = address;
+    }
     
     /** Return the capacity of this rental location
      * @return the capacity of this rental location
      */
-    public int getCapacity();
+    public int getCapacity(){
+        return capacity;
+    }
     
     /** Set the capacity of this rental location
      * @param capacity the new capacity of this rental location
      */
-    public void setCapacity( int capacity );
+    public void setCapacity( int capacity ){
+        this.capacity = capacity;
+    }
 }

fc8953a9dbb83a5f3f0f476a3f0fe62dca2d27c7
bfuster1 <bfuster1@uga.edu>
2015-11-04 23:38:25 -0500

    Update ReservationImpl.java

--- src/edu/uga/cs/rentaride/entity/impl/ReservationImpl.java
+++ src/edu/uga/cs/rentaride/entity/impl/ReservationImpl.java
@@ -1,4 +1,4 @@
-package edu.uga.cs.rentaride.entity;
+package edu.uga.cs.rentaride.entity.impl;
 
 import java.util.Date;
 
@@ -12,61 +12,94 @@ import edu.uga.cs.rentaride.persistence.Persistable;
  * Once the customer actually rents a vehicle, the reservation will involve a rental object.
  *
  */
-public interface Reservation 
-    extends Persistable
+public class ReservationImpl
+    extends Persistent
+    implements Reservation
 {
+    
+    // Reservatio attributes
+    private Date pickupTime;
+    private int rentalDuration;
+    private Customer customer;
+    private VehicleType vehhicleType;
+    private RentalLocation rentalLocation;
+    private Rental rental;
     /** Return the intended pickup time.
      * @return the pickup time for this reservation
      */
-    public Date           getPickupTime();
+    public Date           getPickupTime(){
+        return pickupTime;
+    }
     
     /** Set the intended pickup time.
      * @param pickupTime the new pickup time for this reservation
      * @throws RARException in case the pickup time is in the past
      */
-    public void           setPickupTime( Date pickupTime ) throws RARException;
+    public void           setPickupTime( Date pickupTime ) throws RARException{
+        this.pickupTime = pickupTime;
+    }
     
     /** Return the intended rental duration (in hours).
      * @return the intended rental duration (in hours)
      */
-    public int            getRentalDuration();
+    public int            getRentalDuration(){
+        return rentalDuration;
+    }
     
     /** Set the intended rental duration (in hours).
      * @param rentalDuration the new rental duration (in hours)
      * @throws RARException in case duration time is illegal
      */
-    public void           setRentalDuration( int rentalDuration ) throws RARException;
+    public void           setRentalDuration( int rentalDuration ) throws RARException{
+        this.rentalDuration = rentalDuration;
+    }
     
     /**
      * @return
      */
-    public Customer       getCustomer();
+    public Customer       getCustomer(){
+        return customer;
+    }
     /**
      * @param customer
      */
-    public void           setCustomer( Customer customer );
+    public void           setCustomer( Customer customer ){
+        this.customer = customer;
+    }
     /**
      * @return
      */
-    public VehicleType    getVehicleType();
+    public VehicleType    getVehicleType(){
+        return vehicleType;
+    }
     /**
      * @param vehicleType
      */
-    public void           setVehicleType( VehicleType vehicleType );
+    public void           setVehicleType( VehicleType vehicleType ){
+        this.vehicleType = vehicleType;
+    }
     /**
      * @return
      */
-    public RentalLocation getRentalLocation();
+    public RentalLocation getRentalLocation(){
+        return rentalLocation;
+    }
     /**
      * @param rentalLocation
      */
-    public void           setRentalLocation( RentalLocation rentalLocation );
+    public void           setRentalLocation( RentalLocation rentalLocation ){
+        this.rentalLocation = rentalLocation;
+    }
     /**
      * @return
      */
-    public Rental         getRental();
+    public Rental         getRental(){
+        return rental;
+    }
     /**
      * @param rental
      */
-    public void           setRental( Rental rental );
+    public void           setRental( Rental rental ){
+        this.rental = rental;
+    }
 }

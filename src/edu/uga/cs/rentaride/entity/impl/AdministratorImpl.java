13aa055be93d24c2cdc8dae7fc14b072ac705615
lclapper <lclapper@uga.edu>
2015-11-11 20:52:15 -0500

    Update AdministratorImpl.java

--- src/edu/uga/cs/rentaride/entity/impl/AdministratorImpl.java
+++ src/edu/uga/cs/rentaride/entity/impl/AdministratorImpl.java
@@ -3,15 +3,24 @@ package edu.uga.cs.rentaride.entity.impl;
 import edu.uga.cs.rentaride.entity.RARException;
 import edu.uga.cs.rentaride.entity.Administrator;
 import edu.uga.cs.rentaride.entity.User;
+import edu.uga.cs.rentaride.entity.UserStatus;
 
 /** This class represents an Administrator user.  It has no additional attributes, and all are inherited from User.
  *
  */
 public class AdministratorImpl
-    extends User      //Dont know if this is right becuase Admin etends User
+    extends User      
     implements Administrator
 {
-    //still need to implement the constructor!!
-    //use the setters from the User class
-    //this class does not extend persistent so figure out how to set persistence to (-1)
+    public AdministratorImpl(String fName, String lName, String uName, 
+                    String email, String password, Date createDate, UserStatus userStatus){
+         super.setFirstName(fName);
+         super.setLastName(lName);
+         super.setUserName(uName);
+         super.setPassword(password);
+         super.setEmailAddress(email);
+         super.setCreatedDate(createDate);
+         super.setUserStatus(userStatus);
+         //HOW TO DO PERSISTENCE!!!!!!!!!
+     }
 }

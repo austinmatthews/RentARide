1f962e8e96cb64dde5b9af8ed7ee916150f051f7
lclapper <lclapper@uga.edu>
2015-11-13 19:37:06 -0500

    Update EntityFactoryImpl.java

--- src/edu/uga/cs/rentaride/entity/impl/EntityFactoryImpl.java
+++ src/edu/uga/cs/rentaride/entity/impl/EntityFactoryImpl.java
@@ -1,8 +1,20 @@
-package edu.uga.cs.rentaride.entity;
+package edu.uga.cs.rentaride.entity.impl;
 
 import java.util.Date;
+import edu.uga.cs.rentaride.RARException;
+import edu.uga.cs.rentaride.entity.Administrator;
+import edu.uga.cs.rentaride.entity.Comment;
+import edu.uga.cs.rentaride.entity.Customer;
+import edu.uga.cs.rentaride.entity.HourlyPrice;
+import edu.uga.cs.rentaride.entity.Rental;
+import edu.uga.cs.rentaride.entity.RentalLocation;
+import edu.uga.cs.rentaride.entity.Reservation;
+import edu.uga.cs.rentaride.entity.Vehicle;
+import edu.uga.cs.rentaride.entity.VehicleCondition;
+import edu.uga.cs.rentaride.entity.VehicleStatus;
+import edu.uga.cs.rentaride.entity.VehicleType;
 
-public interface EntityFactory
+public class EntityFactory
 {
     public Administrator createAdministrator( String firstName, String lastName, String userName, 
                                               String emailAddress, String password, Date createDate, 

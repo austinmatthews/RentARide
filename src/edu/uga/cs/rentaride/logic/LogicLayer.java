fb2d97aa4070325c758874ad8678d1f9832bccce
lclapper <lclapper@uga.edu>
2015-12-05 15:56:44 -0500

    Update LogicLayer.java

--- src/edu/uga/cs/rentaride/logic/LogicLayer.java
+++ src/edu/uga/cs/rentaride/logic/LogicLayer.java
@@ -1,17 +1,16 @@
-package edu.uga.clubs.logic;
+package edu.uga.cs.rentaride.logic;
 
 
 import java.util.List;
 
-import edu.uga.clubs.ClubsException;
-import edu.uga.clubs.entity.Club;
-import edu.uga.clubs.entity.Person;
-import edu.uga.clubs.session.Session;
-
+import edu.uga.cs.rentaride.RARException;
+import edu.uga.cs.rentaride.entity.RentalLocation;
+import edu.uga.cs.rentaride.entity.User;
+import edu.uga.cs.rentaride.session.Session;
 
 public interface LogicLayer
 {
-    public List<Club>         findAllClubs() throws ClubsException;
+   /* public List<Club>         findAllClubs() throws ClubsException;
     public List<Person>       findAllPersons() throws ClubsException;
     public long               joinClub( Person person, Club club ) throws ClubsException;
     public long               joinClub( long personId, String clubName ) throws ClubsException;
@@ -21,4 +20,6 @@ public interface LogicLayer
     public List<Person>       findClubMembers( String clubName ) throws ClubsException;
     public void               logout( String ssid ) throws ClubsException;
     public String             login( Session session, String userName, String password ) throws ClubsException;
+    */
+    public long               createRentalLocation(String name, String addr, int capacity) throws RARException;
 }

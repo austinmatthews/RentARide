de015a09724ada71d22b7dc979b23818a03738f3
lclapper <lclapper@uga.edu>
2015-12-06 14:32:39 -0500

    Update FindAllRentalLocations.java

--- src/edu/uga/cs/rentaride/presentation/FindAllRentalLocations.java
+++ src/edu/uga/cs/rentaride/presentation/FindAllRentalLocations.java
@@ -27,8 +27,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import edu.uga.cs.rentaride.entity.Club;
-import edu.uga.cs.rentaride.entity.Person;
+import edu.uga.cs.rentaride.entity.RentalLocation;
 import edu.uga.cs.rentaride.logic.LogicLayer;
 import edu.uga.cs.rentaride.session.Session;
 import edu.uga.cs.rentaride.session.SessionManager;
@@ -53,7 +52,7 @@ extends HttpServlet
     private static final long serialVersionUID = 1L;
 
     static  String            templateDir = "WEB-INF/templates";
-    static  String            resultTemplateName = "FindAllClubs-Result.ftl";
+    static  String            resultTemplateName = "FindAllRentalLocations-Result.ftl";
 
     private Configuration     cfg;
 
@@ -69,16 +68,16 @@ extends HttpServlet
     public void doGet( HttpServletRequest  req, HttpServletResponse res )
             throws ServletException, IOException
     {
-        Template            resultTemplate = null;
-        BufferedWriter      toClient = null;
-        LogicLayer          logicLayer = null;
-        List<Club>          rv = null;
-        List<List<Object>>  clubs = null;
-        List<Object>        club = null;
-        Club   	            c  = null;
-        HttpSession         httpSession;
-        Session             session;
-        String              ssid;
+        Template                resultTemplate = null;
+        BufferedWriter          toClient = null;
+        LogicLayer              logicLayer = null;
+        List<RentalLocation>    rv = null;
+        List<List<Object>>      rentalLocations = null;
+        List<Object>            rentalLocation = null;
+        RentalLocation   	    c  = null;
+        HttpSession             httpSession;
+        Session                 session;
+        String                  ssid;
 
         
         // Load templates from the WEB-INF/templates directory of the Web app.
@@ -103,25 +102,25 @@ extends HttpServlet
         
         httpSession = req.getSession();
         if( httpSession == null ) {       // assume not logged in!
-            ClubsError.error( cfg, toClient, "Session expired or illegal; please log in" );
+            RARError.error( cfg, toClient, "Session expired or illegal; please log in" );
             return;
         }
         
         ssid = (String) httpSession.getAttribute( "ssid" );
         if( ssid == null ) {       // not logged in!
-            ClubsError.error( cfg, toClient, "Session expired or illegal; please log in" );
+            RARError.error( cfg, toClient, "Session expired or illegal; please log in" );
             return;
         }
 
         session = SessionManager.getSessionById( ssid );
         if( session == null ) {
-            ClubsError.error( cfg, toClient, "Session expired or illegal; please log in" );
+            RARError.error( cfg, toClient, "Session expired or illegal; please log in" );
             return; 
         }
         
         logicLayer = session.getLogicLayer();
         if( logicLayer == null ) {
-            ClubsError.error( cfg, toClient, "Session expired or illegal; please log in" );
+            RARError.error( cfg, toClient, "Session expired or illegal; please log in" );
             return; 
         }
         
@@ -134,28 +133,24 @@ extends HttpServlet
         Map<String,Object> root = new HashMap<String,Object>();
         
         try {
-            rv = logicLayer.findAllClubs();
+            rv = logicLayer.findAllRentalLocations();
 
             // Build the data-model
             //
-            clubs = new LinkedList<List<Object>>();
-            root.put( "clubs", clubs );
+            rentalLocations = new LinkedList<List<Object>>();
+            root.put( "rentalLocations", rentalLocations );
 
             for( int i = 0; i < rv.size(); i++ ) {
-                c = (Club) rv.get( i );
-                //Person founder = objectModel.findEstablishedBy( c );
-                Person founder = c.getFounder();
-                club = new LinkedList<Object>();
-                club.add( c.getId() );
-                club.add( c.getName() );
-                club.add( c.getAddress() );
-                club.add( c.getEstablishedOn().toString() );
-                club.add( founder.getFirstName() + " " + founder.getLastName() );
-                clubs.add( club );
+                c = (RentalLocation) rv.get( i );
+                rentalLocation = new LinkedList<Object>();
+                rentalLocation.add( c.getName() );
+                rentalLocation.add( c.getAddress() );
+                rentalLocation.add( c.getCapacity() );
+                rentalLocations.add( rentalLocation );
             }
         } 
         catch( Exception e) {
-            ClubsError.error( cfg, toClient, e );
+            RARError.error( cfg, toClient, e );
             return;
         }
 

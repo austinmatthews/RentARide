952a971f8b335b5ed3c57b56260b10c1162278d6
bfuster1 <bfuster1@uga.edu>
2015-12-07 17:45:29 -0500

    Update CreateVehicleCtrl.java

--- src/edu/uga/cs/rentaride/logic/impl/CreateVehicleCtrl.java
+++ src/edu/uga/cs/rentaride/logic/impl/CreateVehicleCtrl.java
@@ -27,13 +27,17 @@ public class CreateVehicleCtrl {
     }
     
     public long createVehicle( String registrationTag, Date lastService, String make, 
-                               int mileage, String model, String rentalLocation, ENUM status, 
-                               String vehicleType, int vehicleYear, ENUM vehicleCondition)
+                               int mileage, String model, String rl_str, String status_str, 
+                               String vehicleType_str, int vehicleYear, String vehicleCondition_str)
             throws RARException
     { 
-        Vehicle 	           vehicle  = null;
-        Vehicle              modelVehicle = null;
-        Iterator<Vehicle>    vehicleIter = null;
+        Vehicle 	       vehicle  = null;
+        Vehicle            modelVehicle = null;
+        Iterator<Vehicle>  vehicleIter = null;
+        RentalLocation  rentalLocation  = null;
+        Status          status          = null;
+        VehicleType     vehicleType     = null;
+        VehicleCondition vehicleCondition = null;
 
         // check if a vehicle with this name already exists (name is unique)
         modelVehicle = objectLayer.createVehicle();
@@ -43,6 +47,11 @@ public class CreateVehicleCtrl {
             throw new RARException( "A vehicle with this registration tag already exists: " + registrationTag );
 
         // create the vehicle
+        rentalLocation = objectLayer.createRentalLocation("rl_str");
+        status = objectLayer.createStatus("status_str");
+        vehicleType = objectLayer.createVehicleType("vehicleType_str");
+        vehicleCondition = objectLayer.createVehicleContion("vehicleCondition_str");
+        
         vehicle = objectLayer.createVehicle( registrationTag, lastService, make, mileage, model, rentalLocation, status, vehicleType,
                                               vehicleYear, vehicleCondition);
         objectLayer.storeVehicle( vehicle );

79bcb966a05827a42b67188b2bbfc725163c1b18
bfuster1 <bfuster1@uga.edu>
2015-12-08 08:33:37 -0500

    Update ViewCustomerInfo.java

--- src/edu/uga/cs/rentaride/logic/impl/ViewCustomerInfo.java
+++ src/edu/uga/cs/rentaride/logic/impl/ViewCustomerInfo.java
@@ -1,1 +1,48 @@
 
+//
+// A control class to implement the 'View customer info' use case
+//
+//
+
+
+package edu.uga.cs.rentaride.logic.impl;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.uga.uga.cs.RARException;
+import edu.uga.uga.cs.entity.RentalLocation;
+import edu.uga.uga.cs.object.ObjectLayer;
+
+
+public class ViewCustomerInfoCtrl {
+    
+    private ObjectLayer objectLayer = null;
+    
+    public ViewCustomerInfoCtrl( ObjectLayer objectModel )
+    {
+        this.objectLayer = objectModel;
+    }
+
+    public List<User> ViewCustomerInfo()
+            throws RARException
+    {
+        List<User> 	    users  = null;
+        Iterator<User> 	userIter = null;
+        User     	      user = null;
+
+        users = new LinkedList<User>();
+        
+        // retrieve all User objects
+        //
+        userIter = objectLayer.ViewCustomerInfo( null );
+        while( userIter.hasNext() ) {
+            user = userIter.next();
+            System.out.println( user);
+            users.add( user );
+        }
+
+        return users;
+    }
+}
